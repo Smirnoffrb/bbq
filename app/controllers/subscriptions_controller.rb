9be50a349cece0c12185721ff2ab3e4c993e4511
smirnoff <smrnvmail@gmail.com>
2022-05-27 16:34:55 +0300

    change subscription controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,70 +1,34 @@
 class SubscriptionsController < ApplicationController
-  before_action :set_subscription, only: %i[ show edit update destroy ]
+  before_action :set_event, only: %i[create destroy]
+  before_action :set_subscription, only: %i[destroy]
 
-  # GET /subscriptions or /subscriptions.json
-  def index
-    @subscriptions = Subscription.all
-  end
-
-  # GET /subscriptions/1 or /subscriptions/1.json
-  def show
-  end
-
-  # GET /subscriptions/new
-  def new
-    @subscription = Subscription.new
-  end
-
-  # GET /subscriptions/1/edit
-  def edit
-  end
-
-  # POST /subscriptions or /subscriptions.json
   def create
     @subscription = Subscription.new(subscription_params)
 
-    respond_to do |format|
-      if @subscription.save
-        format.html { redirect_to subscription_url(@subscription), notice: "Subscription was successfully created." }
-        format.json { render :show, status: :created, location: @subscription }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @subscription.errors, status: :unprocessable_entity }
-      end
+    if @subscription.save
+      redirect_to @subscription, notice: "Subscription was successfully created."
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /subscriptions/1 or /subscriptions/1.json
-  def update
-    respond_to do |format|
-      if @subscription.update(subscription_params)
-        format.html { redirect_to subscription_url(@subscription), notice: "Subscription was successfully updated." }
-        format.json { render :show, status: :ok, location: @subscription }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @subscription.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /subscriptions/1 or /subscriptions/1.json
   def destroy
     @subscription.destroy
-
-    respond_to do |format|
-      format.html { redirect_to subscriptions_url, notice: "Subscription was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to subscriptions_url, notice: "Subscription was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_subscription
-      @subscription = Subscription.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def subscription_params
-      params.fetch(:subscription, {})
-    end
+  def set_subscription
+    @subscription = @event.subscriptions.find(params[:id])
+  end
+
+  def set_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def subscription_params
+    # .fetch разрешает в params отсутствие ключа :subscription
+    params.fetch(:subscription, {}).permit(:user_email, :user_name)
+  end
 end
