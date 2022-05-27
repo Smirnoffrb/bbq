104c2d03b353a020d74a2e167d4f925f553accf3
smirnoff <smrnvmail@gmail.com>
2022-05-27 16:44:15 +0300

    create partial for subscription and add inf in event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,7 @@ class Event < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
+  has_many :subscribers, through: :subscriptions, source: :user
 
   validates :address, presence: true
   validates :datetime, presence: true
