1ca1f7092ecd5fd691d65e1dd24bb9f1a89ef786
smirnoff <smrnvmail@gmail.com>
2022-05-27 12:03:29 +0300

    generate model comment. add connections to user, event and comment. generate comments controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :user
 
+  has_many :comments
+
   validates :address, presence: true
   validates :datetime, presence: true
   validates :title, presence: true, length: { maximum: 255 }
