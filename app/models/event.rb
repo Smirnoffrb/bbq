1badbba77eb8d401c01aeaff26f88cd0835f6a43
smirnoff <smrnvmail@gmail.com>
2022-05-30 16:35:01 +0300

    change last migration, add subscriptions, change views

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,9 @@ class Event < ApplicationRecord
 
   has_many :comments, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
-  has_many :subscribers, through: :subscriptions
+  has_many :subscribers, through: :subscriptions, source: :user
 
+  validates :user, presence: true
   validates :address, presence: true
   validates :datetime, presence: true
   validates :title, presence: true, length: { maximum: 255 }
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,26 +1,19 @@
 class Subscription < ApplicationRecord
   belongs_to :event
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :event, presence: true
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+
   def user_name
-    if user.present?
-      user.name
-    else
-      super
-    end
+    user&.name || super
   end
 
-  # Если есть юзер, выдаем его email,
-  # если нет – дергаем исходный метод
   def user_email
-    if user.present?
-      user.email
-    else
-      super
-    end
+    user&.email || super
   end
 end
