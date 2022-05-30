67ac442bb34da641a0dd5c87f44094968d2613bd
smirnoff <smrnvmail@gmail.com>
2022-05-30 14:46:37 +0300

    other changes in models and routes

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,16 +3,6 @@ class Subscription < ApplicationRecord
   belongs_to :user
 
   validates :event, presence: true
-
-  # Для конкретного event_id один юзер может подписаться только один раз (если юзер задан)
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
-
-  # Или один email может использоваться только один раз (если анонимная подписка)
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
-
-  # Проверки user_name и user_email выполняются,
-  # только если user не задан
-  # То есть для анонимных пользователей
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
 
   resources :events do
     resources :comments, only: %i[create destroy]
-    resources :subscriptions, only: %i[create destroy]
   end
   resources :users, only: %i[show edit update]
 end
