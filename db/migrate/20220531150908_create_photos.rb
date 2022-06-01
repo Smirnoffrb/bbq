bcc86b0275e7727789967a82fc2d657729807ada
smirnoff <smrnvmail@gmail.com>
2022-06-01 12:43:20 +0300

    fixes

--- db/migrate/20220531150908_create_photos.rb
+++ db/migrate/20220531150908_create_photos.rb
@@ -2,8 +2,8 @@ class CreatePhotos < ActiveRecord::Migration[7.0]
   def change
     create_table :photos do |t|
       t.string :photo
-      t.references :event, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :event, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
 
       t.timestamps
     end
